172fdea81fd52b6b4646297cdd6bc34e76f5d411
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2022-04-11 00:47:32 +0000

    Fix devcontainer and add wasi target

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,4 +1,6 @@
+rustup target add wasm32-wasi
+
 wget https://github.com/WebAssembly/binaryen/releases/download/version_105/binaryen-version_105-x86_64-linux.tar.gz
-tar -xvzf binaryen-version_105-x86_64-linux.tar.gz -C ../
-mv ../binaryen-version_105 ../binaryen
+tar -xvzf binaryen-version_105-x86_64-linux.tar.gz
+mv ./binaryen-version_105 ./binaryen
 rm binaryen-version_105-x86_64-linux.tar.gz
