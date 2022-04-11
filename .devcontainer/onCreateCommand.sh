f4c540dc9f14dce6ca8dd6b5efd1caba25387878
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2022-04-11 00:04:18 +0000

    Add binaryen to devcontainer

---
+++ .devcontainer/onCreateCommand.sh
@@ -0,0 +1,4 @@
+wget https://github.com/WebAssembly/binaryen/releases/download/version_105/binaryen-version_105-x86_64-linux.tar.gz
+tar -xvzf binaryen-version_105-x86_64-linux.tar.gz -C ../
+mv ../binaryen-version_105 ../binaryen
+rm binaryen-version_105-x86_64-linux.tar.gz
